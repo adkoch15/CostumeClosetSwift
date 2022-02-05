e7f1784ec8f44c6f819ed3f8a35781aa72c7c08c
adkoch15 <81789856+adkoch15@users.noreply.github.com>
2022-02-04 18:29:52 -0600

    Add files via upload

---
+++ CostumeShop/CostumeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostumeShopApp.swift
+//  CostumeShop
+//
+//  Created by Anthony Koch on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CostumeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
